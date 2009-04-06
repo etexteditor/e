36ed20c86ee6288529e8f5e01343413332390e32
Adam Vandenberg <flangy@gmail.com>
2009-04-06 14:19:30 -0700

    Updated externals and build scripts.

--- external/build_externals_win.cmd
+++ external/build_externals_win.cmd
@@ -1,10 +1,39 @@
-devenv curllib\curllib.sln /Build Debug
-devenv libtommath\libtommath.sln /Build Debug
-devenv libtomcrypt\libtomcrypt.sln /Build Debug
-devenv metakit\win\msvc90\mksrc.sln /Build Debug
-devenv pcre\pcre.sln /Build Debug
-devenv tinyxml\tinyxml.sln /Build Debug
+@echo off
 
+call :BUILD libtommath\libtommath.sln
+echo %RET%
+call :BUILD libtomcrypt\libtomcrypt.sln
+echo %RET%
+call :BUILD curl\lib\curllib.sln
+echo %RET%
+call :BUILD metakit\win\msvc90\mksrc.sln
+echo %RET%
+call :BUILD pcre\pcre.sln
+echo %RET%
+call :BUILD tinyxml\tinyxml.sln
+echo %RET%
+
+goto :EOF
+
+REM Using a .sln might be faster, but don't want to keep all the .vcprojs up-to-date
 pushd wxwidgets\build\msw
 nmake -f makefile.vc BUILD=debug UNICODE=1
 popd
+
+goto :EOF
+
+
+REM **** Subroutines start here ****
+
+:BUILD
+setlocal
+REM Args are: path_to_sln, [config_to_build=DEBUG]
+set CONFIG=%2
+if {%CONFIG%}=={} set CONFIG="DEBUG"
+
+echo Building %1...
+devenv %1 /Build %CONFIG% > build_logs\%~n1.log
+set RET=%ERRORLEVEL%
+taskkill.exe /f /t /im mspdbsrv.exe > nul 2> nul
+endlocal & set RET=%RET%
+goto :EOF

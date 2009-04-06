ed7d696e16f408f04aa510ffb2d824221edeb0fc
Adam Vandenberg <flangy@gmail.com>
2009-04-06 09:55:11 -0700

    Add wxwidgets build, via nmake, to the build script.

--- external/build_externals_win.cmd
+++ external/build_externals_win.cmd
@@ -1,6 +1,10 @@
-devenv curl/lib/curllib.sln /Build Debug
+devenv curllib\curllib.sln /Build Debug
 devenv libtommath\libtommath.sln /Build Debug
 devenv libtomcrypt\libtomcrypt.sln /Build Debug
 devenv metakit\win\msvc90\mksrc.sln /Build Debug
 devenv pcre\pcre.sln /Build Debug
 devenv tinyxml\tinyxml.sln /Build Debug
+
+pushd wxwidgets\build\msw
+nmake -f makefile.vc BUILD=debug UNICODE=1
+popd

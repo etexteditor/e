d56d6b57f68b9d0987f73037e93d0083c68f9c14
Adam Vandenberg <flangy@gmail.com>
2009-04-06 09:54:21 -0700

    Moved tinyxml files to a subfolder; backup previous zips when downloading.

--- external/get_externals_win.sh
+++ external/get_externals_win.sh
@@ -6,6 +6,10 @@ _download()
   echo "Downloading external libraries..."
   echo
   pushd arch
+
+  tempdir=`mktemp -d back.XXXXXX` || exit 1
+  mv * $tempdir
+
   wget http://curl.haxx.se/download/curl-7.18.2.tar.gz
   wget http://libtomcrypt.com/files/crypt-1.11.tar.bz2
   wget http://math.libtomcrypt.com/files/ltm-0.39.tar.bz2
@@ -13,6 +17,7 @@ _download()
   wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-7.6.tar.gz
   wget http://kent.dl.sourceforge.net/sourceforge/tinyxml/tinyxml_2_5_3.tar.gz
   wget http://biolpc22.york.ac.uk/pub/2.8.9/wxWidgets-2.8.9.tar.bz2
+
   popd
 }
 
@@ -69,7 +74,7 @@ _extract_and_patch()
   cp build_msvc/curllib* curl/lib
   cp build_msvc/libtomcrypt* libtomcrypt
   cp build_msvc/libtommath* libtommath
-  cp build_msvc/tinyxml* tinyxml
+  cp build_msvc/tinyxml/* tinyxml
   cp -r build_msvc/metakit/* metakit/win
   cp -r build_msvc/pcre/* pcre
 }

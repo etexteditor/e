67778e3af73532c11d86c73e9d5365e97a76477d
Adam Vandenberg <flangy@gmail.com>
2009-04-20 10:58:44 -0700

    Comments.

--- src/eDocumentPath.cpp
+++ src/eDocumentPath.cpp
@@ -23,7 +23,7 @@ wxString eDocumentPath::WinPathToCygwin(const wxFileName& path) {
 		return fullpath;
 	}
 	
-	// Check if we have a backslash uncpath; convert to forward slash and pass on.
+	// Check if we have a backslash unc path; convert to forward slash and pass on.
 	if (fullpath.StartsWith(wxT("\\\\"))) {
 		for (unsigned int i = 0; i < fullpath.size(); ++i) {
 			if (fullpath[i] == wxT('\\')) fullpath[i] = wxT('/');

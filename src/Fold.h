8749b8f22ed841816f5f6d37ca7e21dbb58f78f3
Adam Vandenberg <flangy@gmail.com>
2009-05-01 17:08:52 -0700

    Fold does not need to know about wxWidgets or vector.

--- src/Fold.h
+++ src/Fold.h
@@ -1,18 +1,6 @@
 #ifndef __FOLD_H__
 #define __FOLD_H__
 
-#include "wx/wxprec.h" // For compilers that support precompilation, includes "wx/wx.h".
-
-// STL can't compile with Level 4
-#ifdef __WXMSW__
-    #pragma warning(push, 1)
-#endif
-#include <vector>
-#ifdef __WXMSW__
-    #pragma warning(pop)
-#endif
-using namespace std;
-
 // This header defines an interface for editor folds.
 // This is implemented by EditorCtrl, and is used by Lines.
 // We define an interface for this to provide a "wedge" for testing Lines

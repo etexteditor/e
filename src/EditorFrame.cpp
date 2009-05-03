b4e042e02a2d531cb4ffe05edfe9a7d76230371b
Adam Vandenberg <flangy@gmail.com>
2009-05-03 11:04:50 -0700

    Added comment asking why settings were being retrieved again.

--- src/EditorFrame.cpp
+++ src/EditorFrame.cpp
@@ -618,6 +618,7 @@ EditorFrame::~EditorFrame() {
 
 void EditorFrame::RestoreState() {
 #ifdef __WXMSW__ //LINUX: removed until wxWidgets rebuild
+	// Shouldn't this function be using m_settings instead of retreiving them again?
 	eSettings& settings = ((eApp*)wxTheApp)->GetSettings();
 	const unsigned int pagecount = settings.GetPageCount();
 

d99abf93032469a306a5f8a8a395d3c7b286daab
Adam Vandenberg <flangy@gmail.com>
2009-05-07 20:39:31 -0700

    Starting new class for creating non-bundle PLists.

---
+++ src/EasyPlistWriter.cpp
@@ -0,0 +1,5 @@
+#include "EasyPlistWriter.h"
+
+EasyPlistWriter::EasyPlistWriter(void){}
+EasyPlistWriter::~EasyPlistWriter(void){}
+
---
+++ src/EasyPlistWriter.h
@@ -0,0 +1,11 @@
+#ifndef __EASYPLISTWRITER_H__
+#define __EASYPLISTWRITER_H__
+
+class EasyPlistWriter
+{
+public:
+	EasyPlistWriter(void);
+	~EasyPlistWriter(void);
+};
+
+#endif // __EASYPLISTWRITER_H__

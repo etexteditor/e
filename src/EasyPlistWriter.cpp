0a6d157b5ea030ff51b550926cf84dbc03b5271f
Adam Vandenberg <flangy@gmail.com>
2009-05-07 21:13:40 -0700

    Create a new document in constructor.

--- src/EasyPlistWriter.cpp
+++ src/EasyPlistWriter.cpp
@@ -14,18 +14,37 @@
 #include <wx/file.h>
 #include <wx/dir.h>
 
-EasyPlistWriter::EasyPlistWriter(void){}
-EasyPlistWriter::~EasyPlistWriter(void){}
+EasyPlistWriter::EasyPlistWriter(void): 
+	m_doc(new TiXmlDocument()) 
+{
+	TiXmlDeclaration* decl = new TiXmlDeclaration( "1.0", "UTF-8", "" );
+	m_doc->LinkEndChild(decl);
 
-static void plist_add_key(TiXmlElement* parent, const char* key) {
+	TiXmlUnknown* dt = new TiXmlUnknown();
+	dt->SetValue("!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"");
+	m_doc->LinkEndChild(dt);
+
+	TiXmlElement* plist = new TiXmlElement( "plist" );
+	plist->SetAttribute("version", "1.0");
+	m_doc->LinkEndChild(plist);
+
+	m_rootDict = new TiXmlElement("dict");
+	plist->LinkEndChild(m_rootDict);
+}
+
+EasyPlistWriter::~EasyPlistWriter(void){
+	if (m_doc) delete m_doc;
+}
+
+void EasyPlistWriter::AddKey(TiXmlElement* parent, const char* key) {
 	TiXmlElement* keytag = new TiXmlElement("key");
 	parent->LinkEndChild(keytag);
 	TiXmlText* text = new TiXmlText(key);
 	keytag->LinkEndChild(text);
 }
 
-static void plist_add_string(TiXmlElement* parent, const char* key, const char* value){
-	plist_add_key(parent, key);
+void EasyPlistWriter::AddString(TiXmlElement* parent, const char* key, const char* value){
+	AddKey(parent, key);
 
 	TiXmlElement* strtag = new TiXmlElement("string");
 	parent->LinkEndChild(strtag);
@@ -33,8 +52,8 @@ static void plist_add_string(TiXmlElement* parent, const char* key, const char* 
 	strtag->LinkEndChild(text);
 }
 
-static void plist_add_list(TiXmlElement* parent, const char* key, const wxArrayString& list) {
-	plist_add_key(parent, key);
+void EasyPlistWriter::AddList(TiXmlElement* parent, const char* key, const wxArrayString& list) {
+	AddKey(parent, key);
 
 	TiXmlElement* array = new TiXmlElement("array");
 	parent->LinkEndChild(array);
@@ -47,8 +66,8 @@ static void plist_add_list(TiXmlElement* parent, const char* key, const wxArrayS
 	}
 }
 
-static TiXmlElement* plist_add_dict(TiXmlElement* parent, const char* key) {
-	plist_add_key(parent, key);
+TiXmlElement* EasyPlistWriter::AddDict(TiXmlElement* parent, const char* key) {
+	AddKey(parent, key);
 
 	TiXmlElement* dict = new TiXmlElement("dict");
 	parent->LinkEndChild(dict);
--- src/EasyPlistWriter.h
+++ src/EasyPlistWriter.h
@@ -3,12 +3,23 @@
 
 #include "wx/wxprec.h" // For compilers that support precompilation, includes "wx/wx.h".
 
+class TiXmlElement;
+class TiXmlDocument;
 
 class EasyPlistWriter
 {
 public:
 	EasyPlistWriter(void);
 	~EasyPlistWriter(void);
+
+	void AddKey(TiXmlElement* parent, const char* key);
+	void AddString(TiXmlElement* parent, const char* key, const char* value);
+	void AddList(TiXmlElement* parent, const char* key, const wxArrayString& list);
+	TiXmlElement* AddDict(TiXmlElement* parent, const char* key);
+
+private:
+	TiXmlDocument* m_doc;
+	TiXmlElement* m_rootDict;
 };
 
 #endif // __EASYPLISTWRITER_H__

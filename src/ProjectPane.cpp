etexteditor/e

commit 0e444518488fbf86b085506370eb2ef4af09cad0

Moved handling of project info into separate class.

src/ProjectPane.h
@@ -41,6 +41,41 @@ using namespace std;
 class EditorFrame;
 class wxDirWatcherEvent;
 
+class ProjectInfoHandler {
+public:
+	void SetRoot(const wxFileName& path);
+	const wxFileName& GetRoot() const {return m_prjPath;};
+	bool HasProject() const {return m_prjPath.IsOk();};
+
+	// Root info
+	const cxProjectInfo& GetRootInfo() const {return m_projectInfo;};
+	void SaveRootInfo() const;
+
+	bool LoadProjectInfo(const wxString& path, bool onlyFilters, cxProjectInfo& projectInfo) const;
+	void SaveProjectInfo(const cxProjectInfo& projectInfo) const;
+
+	bool GetDirAndFileLists(const wxString& path, wxArrayString& dirs, wxArrayString& files) const;
+
+	// Filters
+	void GetFilters(const wxString& path, wxArrayString& incDirs, wxArrayString& excDirs, wxArrayString& incFiles, wxArrayString& excFiles) const;
+	static bool MatchFilter(const wxString& name, const wxArrayString& incFilter, const wxArrayString& excFilter);
+
+	// GotoFile triggers
+	const map<wxString,wxString>& GetTriggers() const {return m_projectInfo.triggers;};
+	void SetTrigger(const wxString& trigger, const wxString& path);
+	void ClearTrigger(const wxString& trigger);
+
+private:
+	// Support functions
+	void SetPlistKey(const char* key, TiXmlElement* parent) const;
+	void SetPlistString(const char* str, TiXmlElement* parent) const;
+	void SetPlistArray(const wxArrayString& stringArray, TiXmlElement* parent) const;
+
+	// Member variables
+	wxFileName m_prjPath;
+	cxProjectInfo m_projectInfo;
+};
+
 class ProjectPane : public wxPanel, public wxThreadHelper {
 public:
 	ProjectPane(EditorFrame& parent, wxWindowID id = wxID_ANY);
@@ -59,25 +94,17 @@ public:
 	const wxFileName& GetProject() const {return m_prjPath;};
 	wxString GetProjectString() const {if (IsRemote()) return m_prjUrl; else return m_prjPath.GetFullPath();};
 	wxArrayString GetSelections() const;
-	const map<wxString,wxString>& GetEnv() const {return m_projectInfo.env;};
-	bool GetDirAndFileLists(const wxString& path, wxArrayString& dirs, wxArrayString& files) const;
+	const map<wxString,wxString>& GetEnv() const {return m_infoHandler.GetRootInfo().env;};
 
 	void RenameItem();
 	void DeleteItems(bool allowUndo);
 	void RefreshDirs();
 	void Upload(const wxString& url, const wxString& path);
 
-	// GotoFile triggers
-	const map<wxString,wxString>& GetTriggers() const {return m_projectInfo.triggers;};
-	void SetTrigger(const wxString& trigger, const wxString& path);
-	void ClearTrigger(const wxString& trigger);
-
-	// Utility functions
-	bool LoadProjectInfo(const wxString& path, bool onlyFilters, cxProjectInfo& projectInfo) const;
-	void SaveProjectInfo(const cxProjectInfo& projectInfo) const;
-	void SaveCurrentProjectInfo() const {SaveProjectInfo(m_projectInfo);};
-	static bool MatchFilter(const wxString& name, const wxArrayString& incFilter, const wxArrayString& excFilter);
-
+	// ProjectInfo
+	ProjectInfoHandler& GetInfoHandler() {return m_infoHandler;};
+	void SaveCurrentProjectInfo() const {m_infoHandler.SaveRootInfo();};
+	
 #ifdef __WXMSW__
 	WXLRESULT MSWWindowProc(WXUINT nMsg, WXWPARAM wParam, WXLPARAM lParam);
 #endif
@@ -125,14 +152,6 @@ private:
 	void ExpandAndSelect(wxTreeItemId item, wxArrayString& expandedDirs, wxArrayString& selections);
 	void GetExpandedDirs(wxTreeItemId item, wxArrayString& dirs);
 	bool IsDirEmpty(const wxString& path) const;
-
-	// Filters
-	void GetFilters(const wxString& path, wxArrayString& incDirs, wxArrayString& excDirs, wxArrayString& incFiles, wxArrayString& excFiles) const;
-
-	// Support functions
-	void SetPlistKey(const char* key, TiXmlElement* parent) const;
-	void SetPlistString(const char* str, TiXmlElement* parent) const;
-	void SetPlistArray(const wxArrayString& stringArray, TiXmlElement* parent) const;
 
 	// Icon retrieval thread
 	void* Entry();
@@ -189,6 +208,7 @@ private:
 	wxButton* m_settingsButton;
 
 	// Member variables
+	ProjectInfoHandler m_infoHandler;
 	EditorFrame& m_parentFrame;
 	wxImageList m_imageList;
 	void* m_dirWatchHandle;
@@ -199,7 +219,6 @@ private:
 	wxFileName m_prjPath;
 	const RemoteProfile* m_remoteProfile;
 	wxString m_prjUrl;
-	cxProjectInfo m_projectInfo;
 	vector<unsigned int> m_freeImages;
 	wxString m_newFolder; // for OnDirChanged
 	wxString m_newFile;	  // for OnDirChanged

src/EditorFrame.cpp
@@ -2688,7 +2688,7 @@ void EditorFrame::OnMenuGotoFile(wxCommandEvent& WXUNUSED(event)) {
 	wxASSERT(editorCtrl);
 
 	if (m_projectPane->HasProject()) {
-		GotoFileDlg dlg(this, *m_projectPane);
+		GotoFileDlg dlg(this, m_projectPane->GetInfoHandler());
 
 		if (dlg.ShowModal() == wxID_OK) {
 			OpenFile(dlg.GetSelection());

src/GotoFileDlg.cpp
@@ -29,7 +29,7 @@ BEGIN_EVENT_TABLE(GotoFileDlg, wxDialog)
 	EVT_IDLE(GotoFileDlg::OnIdle)
 END_EVENT_TABLE()
 
-GotoFileDlg::GotoFileDlg(wxWindow *parent, ProjectPane& project)
+GotoFileDlg::GotoFileDlg(wxWindow *parent, ProjectInfoHandler& project)
 :  wxDialog (parent, -1, _("Go to File"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE|wxRESIZE_BORDER),
    m_project(project), m_isDone(false), m_filesLoaded(false)
 {
@@ -67,7 +67,7 @@ GotoFileDlg::~GotoFileDlg() {
 void GotoFileDlg::OnIdle(wxIdleEvent& event) {
 	if (!m_filesLoaded && m_project.HasProject()) {
 		if (m_dirStack.empty()) {
-			const wxString path = m_project.GetProject().GetPath();
+			const wxString path = m_project.GetRoot().GetPath();
 			m_files.reserve(100);
 
 			BuildFileList(path);
@@ -86,7 +86,7 @@ void GotoFileDlg::OnIdle(wxIdleEvent& event) {
 			while (cont) {
 				const wxString fulldirname = dirState.prefix + dirState.nextDirName;
 
-				if (!dirState.filter || ProjectPane::MatchFilter(dirState.nextDirName, dirState.filter->includeDirs, dirState.filter->excludeDirs)) {
+				if (!dirState.filter || ProjectInfoHandler::MatchFilter(dirState.nextDirName, dirState.filter->includeDirs, dirState.filter->excludeDirs)) {
 					wxLogDebug(fulldirname);
 					BuildFileList(fulldirname);
 					break;
@@ -144,7 +144,7 @@ void GotoFileDlg::BuildFileList(const wxString& path) {
 	int style = wxDIR_FILES;
 	bool cont = dirState->dir.GetFirst(&eachFilename, wxEmptyString, style);
 	while (cont) {
-		if (!dirState->filter || ProjectPane::MatchFilter(eachFilename, dirState->filter->includeFiles, dirState->filter->excludeFiles)) {
+		if (!dirState->filter || ProjectInfoHandler::MatchFilter(eachFilename, dirState->filter->includeFiles, dirState->filter->excludeFiles)) {
 			m_files.push_back(new FileEntry(dirState->prefix, eachFilename));
 		}
 

src/GotoFileDlg.h
@@ -21,7 +21,7 @@
 
 class GotoFileDlg : public wxDialog {
 public:
-	GotoFileDlg(wxWindow *parent, ProjectPane& project);
+	GotoFileDlg(wxWindow *parent, ProjectInfoHandler& project);
 	~GotoFileDlg();
 
 	const wxString& GetSelection() const {return m_cmdList->GetSelectedAction()->path;};
@@ -104,7 +104,7 @@ private:
 	};
 
 	// Member variables
-	ProjectPane& m_project;
+	ProjectInfoHandler& m_project;
 	vector<FileEntry*> m_files;
 	bool m_isDone;
 

commit 998aaa7d1797da6df1c862615661b2fa1859d7e2

Fixes error introduced in 0e444518488fbf86b085506370eb2ef4af09cad0

src/ProjectPane.cpp
@@ -1861,7 +1861,7 @@ void ProjectPane::WatchTree(const wxString &path) {
 	wxArrayString excludeDirs;
 	wxArrayString includeFiles;
 	wxArrayString excludeFiles;
-	GetFilters(path, includeDirs, excludeDirs, includeFiles, excludeFiles);
+	m_infoHandler.GetFilters(path, includeDirs, excludeDirs, includeFiles, excludeFiles);
 
 	// Get all subdirs
 	wxString CurrentDir;
@@ -1870,7 +1870,7 @@ void ProjectPane::WatchTree(const wxString &path) {
 		do {
 			if (eDocumentPath::IsDotDirectory(CurrentDir)) continue;
 
-			if (MatchFilter(CurrentDir, includeDirs, excludeDirs)) {
+			if (ProjectInfoHandler::MatchFilter(CurrentDir, includeDirs, excludeDirs)) {
 				// watch dir and all subdirs
 				wxString FullPath = path + wxT("/") + CurrentDir;
 				m_parentFrame.GetDirWatcher().WatchDirectory(FullPath, *this, true);

src/ProjectPane.h
@@ -130,7 +130,6 @@ private:
 	static bool GetDefaultIcon(wxIcon &icon);
 	void WatchTree(const wxString &path);
 #endif
-	static bool MatchFilter(const wxString& name, const wxArrayString& incFilter, const wxArrayString& excFilter);
 
 	void Init();
 
@@ -152,9 +151,6 @@ private:
 	void ExpandAndSelect(wxTreeItemId item, wxArrayString& expandedDirs, wxArrayString& selections);
 	void GetExpandedDirs(wxTreeItemId item, wxArrayString& dirs);
 	bool IsDirEmpty(const wxString& path) const;
-
-	// Filters
-	void GetFilters(const wxString& path, wxArrayString& incDirs, wxArrayString& excDirs, wxArrayString& incFiles, wxArrayString& excFiles) const;
 
 	// Icon retrieval thread
 	void* Entry();
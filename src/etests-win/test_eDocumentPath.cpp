7bd75fa689e2e43e07e93ea3374e771ea10daa29
Adam Vandenberg <flangy@gmail.com>
2009-05-13 15:00:15 -0700

    Added simple remote path tests.

--- src/etests-win/test_eDocumentPath.cpp
+++ src/etests-win/test_eDocumentPath.cpp
@@ -3,15 +3,17 @@
 #include "eDocumentPath.h"
 #include <gtest/gtest.h>
 
-/*
-bool eDocumentPath::IsDotDirectory(const wxString& path) {
-	return path == wxT(".") || path == wxT("..");
-}
-*/
-
-
 TEST(eDocumentPathTest, isDotDirectory) {
   EXPECT_TRUE( eDocumentPath::IsDotDirectory(wxT(".")) );
   EXPECT_TRUE( eDocumentPath::IsDotDirectory(wxT("..")) );
+
   EXPECT_FALSE( eDocumentPath::IsDotDirectory(wxT(".git")) );
+}
+
+
+TEST(eDocumentPathTest, isRemotepath) {
+	EXPECT_TRUE( eDocumentPath::IsRemotePath(wxT("ftp://example.com/ftp/folder/file.txt")) );
+	EXPECT_TRUE( eDocumentPath::IsRemotePath(wxT("http://example.com/ftp/folder/file.txt")) );
+
+	EXPECT_FALSE( eDocumentPath::IsRemotePath(wxT("c:\\Program Files\\SomeFile.txt")) );
 }

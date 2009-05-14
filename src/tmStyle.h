7bd7a06d95a65120d9e853bb29b2291f9e7729d1
Adam Vandenberg <flangy@gmail.com>
2009-05-13 20:18:16 -0700

    Missing style include; oops.

---
+++ src/tmStyle.h
@@ -0,0 +1,12 @@
+#ifndef __TMSTYLE_H__
+#define __TMSTYLE_H__
+
+struct style {
+	wxString name;
+	wxString scope;
+	wxColour foregroundcolor;
+	wxColour backgroundcolor;
+	int fontflags;
+};
+
+#endif // __TMSTYLE_H__

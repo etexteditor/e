6934abf44dc4fb90b77c9b0c78a891696fa55f93
Adam Vandenberg <flangy@gmail.com>
2009-06-09 21:51:14 -0700

    Add unit tests for colour parsing.

--- src/Colours.cpp
+++ src/Colours.cpp
@@ -3,12 +3,13 @@
 
 bool ParseColourAlpha(const char* text, wxColour& colour, unsigned int& alpha) {
 	if (!text) return false;
-	if (strlen(text) < 7) return false;
+
+	size_t digits = strlen(text);
+	if (digits != 7 && digits != 9) return false;
+
 	if (text[0] != '#') return false;
 
-	int red;
-	int green;
-	int blue;
+	int red, green, blue;
 	alpha = 0;
 	sscanf(text, "#%2x%2x%2x%2x", &red, &green, &blue, &alpha);
 

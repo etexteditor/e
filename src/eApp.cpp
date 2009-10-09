3bd63ba96d0975f59c55722ff093f3246babb08d
Alexander Stigsen <stigsen@e-texteditor.com>
2009-10-09 13:03:48 +0200

    Prepare for release 1.0.39

--- src/eApp.cpp
+++ src/eApp.cpp
@@ -83,8 +83,8 @@ bool eApp::OnInit() {
 
 	// App info
 	const wxString appId = wxString::Format(wxT("eApp-%s"), wxGetUserId().c_str());
-	m_version_id = 202;  // <-------------- INTERNAL VERSION NUMBER
-	m_version_name =  wxT("1.0.38"); // <-- VERSION NAME
+	m_version_id = 205;  // <-------------- INTERNAL VERSION NUMBER
+	m_version_name =  wxT("1.0.39"); // <-- VERSION NAME
 
 	// Option vars
 	m_lineNum = 0;

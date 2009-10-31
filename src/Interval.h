9845d8921880c1e7fec2defe20d5e4272a62fb2f
Adam Vandenberg <flangy@gmail.com>
2009-10-31 11:03:55 -0700

    Add method to Interval, mark all methods 'inline'.

--- src/Interval.h
+++ src/Interval.h
@@ -3,13 +3,19 @@
 
 class interval {
 public:
-	interval() : start((unsigned int)-1), end((unsigned int)-1) {};
-	interval(unsigned int s, unsigned int e) : start(s), end(e) {};
-	bool operator==(const interval& iv) const {return (start == iv.start && end == iv.end);};
-	bool operator!=(const interval& iv) const {return (start != iv.start || end != iv.end);};
-	bool empty() const { return start >= end; };
-	void Set(unsigned int s, unsigned int e) {start = s; end = e;};
-	inline void Get(unsigned int& s, unsigned int& e) const {s = start; e = end;};
+	interval() : start((unsigned int)-1), end((unsigned int)-1) {}
+	interval(unsigned int s, unsigned int e) : start(s), end(e) {}
+
+	inline bool operator==(const interval& iv) const {return (start == iv.start && end == iv.end);}
+	inline bool operator!=(const interval& iv) const {return (start != iv.start || end != iv.end);}
+
+	inline bool empty() const { return start >= end; }
+
+	inline void Set(unsigned int s, unsigned int e) {start = s; end = e;}
+	inline void Get(unsigned int& s, unsigned int& e) const {s = start; e = end;}
+
+	inline bool IsPoint(unsigned int p) const { return p == start && p == end; }
+
 	unsigned int start;
 	unsigned int end;
 };

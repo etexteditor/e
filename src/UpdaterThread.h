86366946842f96596ac614410995eed50176497e
Vlad Ershov <vladimir.a.ershov@gmail.com>
2010-10-25 18:32:38 +0400

    FS#433 Update last checking time

--- src/UpdaterThread.h
+++ src/UpdaterThread.h
@@ -1,6 +1,8 @@
 #ifndef __UPDATERTHREAD_H__
 #define __UPDATERTHREAD_H__
 
+#include <wx/thread.h>
+
 // Constants
 enum {
 	ID_UPDATES_AVAILABLE=100,
@@ -9,6 +11,25 @@ enum {
 
 class ISettings;
 class AppVersion;
-void CheckForUpdates(ISettings& settings, AppVersion* info, bool forceCheck=false);
+class wxHTTP;
+class AppVersion;
+class SettingsDlg;
+
+class UpdaterThread : public wxThread {
+public:
+	UpdaterThread(wxHTTP* http, AppVersion* info);
+	virtual void *Entry();
+	// set dialog to be notified after checking or NULL
+	void SetWatcher(SettingsDlg *dlg);
+
+private:
+	wxHTTP* m_http;
+	AppVersion* m_info;
+
+	wxCriticalSection m_CritSectWatcher; // locker for m_Watcher ptr
+	SettingsDlg *m_Watcher;
+};
+
+UpdaterThread* CheckForUpdates(ISettings& settings, AppVersion* info, bool forceCheck=false);
 
 #endif
